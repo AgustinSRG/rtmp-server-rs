74e039e6f6d26e3c6a7dfcb9128b066ed5d64d92
AgustinSRG <agustinsanromanguzman@gmail.com>
2025-01-31 17:46:31 +0100

    TLS config auto reload system

--- src/server/tls.rs
+++ src/server/tls.rs
@@ -2,9 +2,14 @@
 
 use std::net::IpAddr;
 use std::sync::Arc;
+use std::time::Duration;
 
+use filetime::FileTime;
+use rustls::server::ResolvesServerCert;
+use rustls::sign::{CertifiedKey, SigningKey};
 use tokio::io::AsyncWriteExt;
 use tokio::net::TcpStream;
+use tokio::sync::mpsc::Receiver;
 use tokio::sync::Mutex;
 use tokio::{net::TcpListener, sync::mpsc::Sender};
 
@@ -31,6 +36,21 @@ pub fn tls_server(
     end_notifier: Sender<()>,
 ) {
     tokio::spawn(async move {
+        let cert_file_metadata = match tokio::fs::metadata(&config.tls.certificate).await {
+            Ok(m) => m,
+            Err(e) => {
+                logger.log_error(&format!("Could not load certificate: {}", e.to_string()));
+                end_notifier
+                    .send(())
+                    .await
+                    .expect("failed to notify to main thread");
+                return;
+            }
+        };
+
+        let cert_file_mod_time =
+            FileTime::from_last_modification_time(&cert_file_metadata).unix_seconds();
+
         let certs_res = CertificateDer::pem_file_iter(&config.tls.certificate);
         let mut certificate: Vec<CertificateDer<'_>> = Vec::new();
 
@@ -52,6 +72,21 @@ pub fn tls_server(
             }
         }
 
+        let key_file_metadata = match tokio::fs::metadata(&config.tls.key).await {
+            Ok(m) => m,
+            Err(e) => {
+                logger.log_error(&format!("Could not load private key: {}", e.to_string()));
+                end_notifier
+                    .send(())
+                    .await
+                    .expect("failed to notify to main thread");
+                return;
+            }
+        };
+
+        let key_file_mod_time =
+            FileTime::from_last_modification_time(&key_file_metadata).unix_seconds();
+
         let key_res = PrivateKeyDer::from_pem_file(&config.tls.key);
         let key: PrivateKeyDer<'_>;
 
@@ -71,27 +106,27 @@ pub fn tls_server(
 
         let listen_addr = config.tls.get_tcp_listen_addr();
 
-        let tls_config_res = rustls::ServerConfig::builder()
-            .with_no_client_auth()
-            .with_single_cert(certificate, key);
-        let tls_config: rustls::ServerConfig;
+        let tls_config_builder = rustls::ServerConfig::builder();
 
-        match tls_config_res {
-            Ok(c) => {
-                tls_config = c;
-            }
+        let key_provider = tls_config_builder.crypto_provider().key_provider;
+
+        let signing_key = match key_provider.load_private_key(key) {
+            Ok(k) => k,
             Err(e) => {
-                logger.log_error(&format!(
-                    "Could not load TLS configuration: {}",
-                    e.to_string()
-                ));
+                logger.log_error(&format!("Could not load private key: {}", e.to_string()));
                 end_notifier
                     .send(())
                     .await
                     .expect("failed to notify to main thread");
                 return;
             }
-        }
+        };
+
+        let cert_resolver = Arc::new(CustomCertResolver::new(certificate, signing_key));
+
+        let tls_config = tls_config_builder
+            .with_no_client_auth()
+            .with_cert_resolver(cert_resolver.clone());
 
         let acceptor = TlsAcceptor::from(Arc::new(tls_config));
 
@@ -114,6 +149,29 @@ pub fn tls_server(
         }
 
         logger.log_info(&format!("Listening on {}", listen_addr));
+
+        // Spawn task to reload certificates periodically
+
+        let cancel_tls_reloader_sender: Option<Sender<()>>;
+
+        if config.tls.check_reload_seconds > 0 {
+            let (cancel_sender, cancel_receiver) = tokio::sync::mpsc::channel::<()>(1);
+
+            spawn_task_periodically_reload_tls_config(
+                cert_file_mod_time,
+                key_file_mod_time,
+                logger.clone(),
+                config.clone(),
+                cert_resolver,
+                cancel_receiver,
+            );
+
+            cancel_tls_reloader_sender = Some(cancel_sender);
+        } else {
+            cancel_tls_reloader_sender = None;
+        }
+
+        // Main loop
 
         loop {
             let accept_res = listener.accept().await;
@@ -138,9 +196,13 @@ pub fn tls_server(
                         .send(())
                         .await
                         .expect("failed to notify to main thread");
-                    return;
+                    break;
                 }
             }
+        }
+
+        if let Some(cancel_sender) = cancel_tls_reloader_sender {
+            _ = cancel_sender.send(());
         }
     });
 }
@@ -220,6 +282,157 @@ fn handle_connection_tls(
                 ));
             }
             let _ = connection.shutdown().await;
+        }
+    });
+}
+
+/// Custom certificate resolver
+#[derive(Debug)]
+struct CustomCertResolver {
+    /// Key + certs
+    pub certified_key: std::sync::Mutex<Arc<CertifiedKey>>,
+}
+
+impl CustomCertResolver {
+    /// Creates new CustomCertResolver
+    pub fn new(cert: Vec<CertificateDer<'static>>, key: Arc<dyn SigningKey>) -> CustomCertResolver {
+        CustomCertResolver {
+            certified_key: std::sync::Mutex::new(Arc::new(CertifiedKey::new(cert, key))),
+        }
+    }
+
+    /// Sets TLS configuration
+    pub fn set_config(&self, cert: Vec<CertificateDer<'static>>, key: Arc<dyn SigningKey>) {
+        let mut certified_key_v = self.certified_key.lock().unwrap();
+        *certified_key_v = Arc::new(CertifiedKey::new(cert, key));
+    }
+}
+
+impl ResolvesServerCert for CustomCertResolver {
+    fn resolve(
+        &self,
+        _client_hello: rustls::server::ClientHello<'_>,
+    ) -> Option<Arc<rustls::sign::CertifiedKey>> {
+        let certified_key_v = self.certified_key.lock().unwrap();
+        Some(certified_key_v.clone())
+    }
+}
+
+fn spawn_task_periodically_reload_tls_config(
+    initial_cert_time: i64,
+    initial_key_time: i64,
+    logger: Arc<Logger>,
+    config: Arc<RtmpServerConfiguration>,
+    cert_resolver: Arc<CustomCertResolver>,
+    mut cancel_receiver: Receiver<()>,
+) {
+    tokio::spawn(async move {
+        let mut cert_time = initial_cert_time;
+        let mut key_time = initial_key_time;
+
+        let mut finished = false;
+
+        while !finished {
+            // Wait
+            tokio::select! {
+                _ = tokio::time::sleep(Duration::from_secs(config.tls.check_reload_seconds as u64)) => {}
+                _ = cancel_receiver.recv() => {
+                    finished = true;
+                    continue;
+                }
+            }
+
+            if logger.config.debug_enabled {
+                logger.log_debug("Checking for changes in TLS configuration...");
+            }
+
+            // Check
+
+            let cert_file_metadata = match tokio::fs::metadata(&config.tls.certificate).await {
+                Ok(m) => m,
+                Err(e) => {
+                    logger.log_error(&format!("Could not load certificate: {}", e.to_string()));
+                    continue;
+                }
+            };
+
+            let cert_file_mod_time =
+                FileTime::from_last_modification_time(&cert_file_metadata).unix_seconds();
+
+            let key_file_metadata = match tokio::fs::metadata(&config.tls.key).await {
+                Ok(m) => m,
+                Err(e) => {
+                    logger.log_error(&format!("Could not load private key: {}", e.to_string()));
+                    continue;
+                }
+            };
+
+            let key_file_mod_time =
+                FileTime::from_last_modification_time(&key_file_metadata).unix_seconds();
+
+            if cert_file_mod_time == cert_time && key_file_mod_time == key_time {
+                if logger.config.debug_enabled {
+                    logger.log_debug("No changes detected in TLS configuration");
+                }
+
+                continue;
+            }
+
+            // Changes detected, reload configuration
+
+            let certs_res = CertificateDer::pem_file_iter(&config.tls.certificate);
+            let mut certificate: Vec<CertificateDer<'_>> = Vec::new();
+
+            match certs_res {
+                Ok(certs_iter) => {
+                    for c in certs_iter {
+                        if let Ok(cert) = c {
+                            certificate.push(cert);
+                        }
+                    }
+                }
+                Err(e) => {
+                    logger.log_error(&format!("Could not load certificate: {}", e.to_string()));
+                    continue;
+                }
+            }
+
+            let key_res = PrivateKeyDer::from_pem_file(&config.tls.key);
+            let key: PrivateKeyDer<'_>;
+
+            match key_res {
+                Ok(k) => {
+                    key = k;
+                }
+                Err(e) => {
+                    logger.log_error(&format!("Could not load private key: {}", e.to_string()));
+                    continue;
+                }
+            }
+
+            let tls_config_builder = rustls::ServerConfig::builder();
+
+            let key_provider = tls_config_builder.crypto_provider().key_provider;
+
+            let signing_key = match key_provider.load_private_key(key) {
+                Ok(k) => k,
+                Err(e) => {
+                    logger.log_error(&format!("Could not load private key: {}", e.to_string()));
+                    continue;
+                }
+            };
+
+            // Update mod times
+            cert_time = cert_file_mod_time;
+            key_time = key_file_mod_time;
+
+            // Update config
+            cert_resolver.set_config(certificate, signing_key);
+
+            // Log
+            if logger.config.info_enabled {
+                logger.log_debug("TLS configuration reloaded");
+            }
         }
     });
 }
